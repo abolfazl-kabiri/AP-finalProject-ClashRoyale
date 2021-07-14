9ca51d5d665903fd6c3957ffd3fe943b24a81112
mohammad-hossein zeynal zadeh <78679020+mh-zeynal@users.noreply.github.com>
2021-07-15 00:31:34 +0430

    Delete towers directory

--- towers/PrincessTower.java
+++
@@ -1,7 +0,0 @@
-package towers;
-
-public class PrincessTower extends Tower{
-    public PrincessTower(int hp, int damage) {
-        super(hp, damage, 0.8, 7.5, true, ".\\photos\\inside game models\\blue queen building_00000.png");
-    }
-}
